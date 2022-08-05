f07bb9193020865b87afa675fc14304dd5d913ef
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-05 21:01:20 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Index({ projects, setTheme }) {
       <Container>
         <Landing />
         <Skills />
-        <Projects data={projects}/>
+//         <Projects data={projects}/>
         <Experience/>
         <About/>
       </Container>
